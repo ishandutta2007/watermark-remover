6af07aabc399e97325b8d9f8b1ddb11afa99883b
lokwhite <whitelok@163.com>
2017-12-28 18:47:06 +0800

    update

--- waterremover.m
+++ waterremover.m
@@ -1,8 +1,22 @@
 clear;
 
+watermarkSize = [623 134];
+display('Watermark size is: ', watermarkSize);
+
+display('Importing relative project path...');
+display('Importing fast-directional-chamfer-matching')
+addpath(genpath('fast-directional-chamfer-matching'));
+
 initialImagesPath = '../watermark-creator/resources/watermarked-images-small';
 initialWatermarkPositionListPath = '../watermark-creator/resources/watermark-position.txt';
-watermarkSize = [623 134];
+
 
 delta_W_lower_m_upper_path = compute_delta_W_lower_m_upper(initialImagesPath, initialWatermarkPositionListPath, watermarkSize);
 
+fullImagesPath = '../watermark-creator/resources/watermarked-images';
+
+test_image = fullfile(fullImagesPath, 'tumblr_luvmtr0zVS1r3rsfmo1_1280.jpg');
+
+
+wins = FDCMChamferDetect(delta_W_lower_m_upper_path, test_image, 'test_result.png');
+display(wins);
